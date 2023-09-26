3856e730f5c5eea6c9ba91ec6cd017c3198d9527
TheAnushervon <anushervon4j@gmail.com>
2023-09-26 21:35:36 +0300

    doing ex3

--- Week03/ex3.c
+++ Week03/ex3.c
@@ -6,6 +6,12 @@
 struct Directory;
 struct File;
 
+typedef void (*Show_file)(struct File* file);
+typedef void (*Show_dir) (struct Directory *dir) ; 
+typedef void (*Add_dir) (struct Direcotry *dir1, struct Directory *dir2); 
+typedef void (*Add_file)(struct File *file, struct Directory* dir); 
+
+
 typedef struct File
 {
     short int id;
@@ -19,13 +25,68 @@ typedef struct Directory
 {
     int nf;
     int nd;
-    File files[256];
+    File files[256]; 
     struct Directory *sub_dirs[MAX_DIR];
     char path[MAX_PATH];
     char name[MAX_NAME];
 
-    //<WRITE YOUR CODE HERE>;
+    Show_dir show_dir ; 
+    Show_file show_file; 
+    Add_dir add_dir ; 
 } Directory;
+
+void show_file(File *file) {
+    printf("%s ", file->file_name) ; 
+}
+
+void show_dir(Directory *dir)
+{
+    printf("\nDIRECTORY\n");
+    printf("path: %s\n", dir->path);
+    printf("files:\n");
+    printf("[ ");
+    
+    for (int i = 0; i < dir->nf; i++)
+    {
+        show_file(&(dir->files[i]));
+    }
+    
+    printf("]\n");
+    printf("directories:\n");
+    printf("{ ");
+    
+    for (int i = 0; i < dir->nd; i++)
+    {
+        show_dir(dir->sub_dirs[i]);
+    }
+    
+    printf("}\n");
+}
+
+void add_dir(Directory *dir1, Directory *dir2)
+{
+    if (dir1 && dir2)
+    {
+        dir2->sub_dirs[dir2->nd] = dir1;
+        dir2->nd++;
+        char temp_path[MAX_PATH];
+        
+        if (strcmp(dir2->path, "/"))
+        {
+            strcpy(temp_path, dir2->path);
+            strcat(temp_path, "/");
+            strcat(temp_path, dir1->name);
+            strcpy(dir1->path, temp_path);
+        }
+        else
+        {
+            strcpy(temp_path, "/");
+            strcat(temp_path, dir1->name);
+            strcpy(dir1->path, temp_path);
+        }
+    }
+}
+
 
 int main(){
 
