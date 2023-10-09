f36ba6bc64259a1ea85a94d51be8b2a01614b365
TheAnushervon <anushervon4j@gmail.com>
2023-10-09 23:23:28 +0300

    solved Part 1, ex1

--- Week05/channel.c
+++ Week05/channel.c
@@ -1,6 +1,7 @@
 #include <stdio.h> 
 #include <unistd.h>
 #include <sys/types.h>
+#include<string.h>
 #define MAX 1025
 int main(int argc, char* argv[]){
 int fd[2]; 
@@ -14,15 +15,15 @@ int id = fork() ;
 if(id == 0) { 
     close(fd[0]) ; 
     char message[MAX] ;
-    printf("Write a message: ") ; 
-    scanf("%s", message); 
-    write(fd[1], &message, sizeof(MAX)); 
+    printf("It is publisher.Write a message: ") ; 
+    fgets(message, MAX,stdin) ; 
+    write(fd[1], &message, strlen(message)+1) ; 
 }else {
     close(fd[1]) ; 
     char message1[MAX] ; 
-    read(fd[0], &message1, sizeof(MAX));
+    read(fd[0], &message1, sizeof(message1));
     close(fd[0]) ; 
-    printf("Here is the message from subscriber: %s", message1);
+    printf("It is subcriber.Here is the message: %s", message1);
 }
 
 }
