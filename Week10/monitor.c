568cc8ff22b7bb41fa20843ce3e34bb0a7e5b226
TheAnushervon <anushervon4j@gmail.com>
2023-11-13 13:18:21 +0300

    added traversing

--- Week10/monitor.c
+++ Week10/monitor.c
@@ -3,12 +3,54 @@
 #include <string.h> 
 #include <signal.h> 
 #include <unistd.h>
+#include <dirent.h> 
+#include <sys/stat.h>
+#include <sys/inotify.h>
+#include <errno.h>
+#include<sys/types.h>
 char *path ; 
 volatile int shouldex = 1 ; 
+DIR *directory; 
+#define EVENT_SIZE (sizeof(struct inotify_event))
+#define EVENT_BUF_LEN (1024 * (EVENT_SIZE + 16))
+
+void traverse(const char *fn)
+{
+    DIR *dir;
+    struct dirent *entry;
+    char path[1024];
+    struct stat info;
+
+    printf("Directory structure of %s:\n", fn);
+
+    if ((dir = opendir(fn)) == NULL)
+        perror("opendir error");
+    else
+    {
+        while ((entry = readdir(dir)) != NULL)
+        {
+            if (entry->d_name[0] != '.')
+            {
+                strcpy(path, fn);
+                strcat(path, "/");
+                strcat(path, entry->d_name);
+
+                if (stat(path, &info) != 0)
+                    fprintf(stderr, "stat() error on %s\n", path);
+
+                printf("I-node number: %lu\n", info.st_ino);
+                printf("Hard links: %lu\n", info.st_nlink);
+            }
+        }
+        closedir(dir);
+    }
+}
+
 void signalHandler(int signum){
     printf("Recieved SIGNINT signal.\n") ; 
    shouldex = 0 ; 
-   statinfo_output() ; // function for output all 
+   traverse(path) ; 
+  // statinfo_output() ; // function for output all 
    // stats of folder 
     exit (EXIT_SUCCESS) ;
     
@@ -17,17 +59,96 @@ int main(int argc, char* argv[]){
    
     path = malloc(strlen(argv[1])+1) ; 
     path = strcpy(path, argv[1]) ; 
-   
+    directory = opendir(path) ; 
     printf("%s", path) ; 
     signal(SIGINT,signalHandler) ; 
-    while(shouldex){printf("d");
+    /*while(shouldex){printf("d");
     fflush(stdout) ; 
-    sleep(1);}
+    sleep(1);}*/
+
     /*
     1) Output stat info of all files 
     and folders in start
     and end 
     2) Some modification should be 
     printed out
-    */
+    */int length, i = 0;
+    int fd;
+    int wd;
+    char buffer[EVENT_BUF_LEN];
+
+  
+
+    /* Print the stat info of all files and folders in the path on startup */
+    traverse(path);
+
+    /* Creating the INOTIFY instance */
+    fd = inotify_init();
+
+    /* Checking for error */
+    if (fd < 0)
+    {
+        perror("inotify_init");
+        return EXIT_FAILURE;
+    }
+
+    /* Adding the specified directory into the watch list. */
+    wd = inotify_add_watch(fd, path, IN_CREATE | IN_DELETE);
+
+    /* Read to determine the event change happens on the specified directory. */
+    length = read(fd, buffer, EVENT_BUF_LEN);
+
+    /* Checking for error */
+    if (length < 0)
+    {
+        perror("read");
+        close(fd);
+        return EXIT_FAILURE;
+    }
+
+    /* Actually read returns the list of change events happens. */
+    while (i < length)
+    {
+        struct inotify_event *event = (struct inotify_event *)&buffer[i];
+        if (event->len)
+        {
+            if (event->mask & IN_CREATE)
+            {
+                if (event->mask & IN_ISDIR)
+                {
+                    printf("New directory %s created.\n", event->name);
+                }
+                else
+                {
+                    printf("New file %s created.\n", event->name);
+                }
+            }
+            else if (event->mask & IN_DELETE)
+            {
+                if (event->mask & IN_ISDIR)
+                {
+                    printf("Directory %s deleted.\n", event->name);
+                }
+                else
+                {
+                    printf("File %s deleted.\n", event->name);
+                }
+            }
+        }
+        i += EVENT_SIZE + event->len;
+    }
+
+    /* Print the stat info of all files and folders in the path before termination */
+    traverse(path);
+
+    /* Removing the specified directory from the watch list. */
+    inotify_rm_watch(fd, wd);
+
+    /* Closing the INOTIFY instance. */
+    close(fd);
+
+    return EXIT_SUCCESS;
+
+   printf("Here"); 
+   traverse(path) ; 
 }
