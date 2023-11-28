fd94bdfb0555280d43de0322086e6b565a6ece0e
TheAnushervon <anushervon4j@gmail.com>
2023-11-28 23:58:40 +0300

    fixed

--- Week12/ex1.c
+++ Week12/ex1.c
@@ -3,26 +3,24 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <linux/input.h>
-#define HISTORY_SIZE 2 
+
 const char *device_path = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
-void check_input (signed int value){
-    if (value == 1){
-        printf("Pressed"); 
-    }
-    else if (value == 2){
-        printf("REPEATED"); 
-    }
-    else printf("RELEASED"); 
-}
+
+#define HISTORY_SIZE 4
+
 struct input_event_history {
     struct input_event events[HISTORY_SIZE];
     int index;
 };
+
+void print_custom_message(const char *message) {
+    printf("Shortcut: %s\n", message);
+}
+
 int main() {
     // Open the keyboard device with sudo permissions
-    
     int fd = open(device_path, O_RDONLY);
-    char saver[3] = {'z', 'z', 'z'}; 
+
     if (fd == -1) {
         perror("Error opening keyboard device");
         return EXIT_FAILURE;
@@ -37,10 +35,11 @@ int main() {
         close(fd);
         return EXIT_FAILURE;
     }
-    else printf("opened file"); 
+
+    // Initialize the input event history
+    struct input_event_history history = {0};
 
     // Main event handling loop
-    struct input_event_history history = {0};
     while (1) {
         // Read the keyboard event
         ssize_t bytesRead = read(fd, &ev, sizeof(struct input_event));
@@ -53,38 +52,59 @@ int main() {
         // Check if it's an EV_KEY event (key press or release)
         if (ev.type == EV_KEY) {
             // Check for PRESSED, REPEATED, or RELEASED events
-            if (ev.value == 1 || ev.value == 2 || ev.value == 0) {
+            if (ev.value == 1||ev.value == 2 || ev.value == 0) {
                 // Print and save the output events in the specified format
                 fprintf(output_file, "%s 0x%04X (%d)\n",
-                        (ev.value == 1) ? "PRESSED" : ((ev.value == 0) ? "RELEASED" : "REPEATED"),
+                        (ev.value == 1) ? "PRESSED" : ((ev.value == 2) ? "REPEATED" : "RELEASED"),
                         ev.code, ev.code);
                 fflush(output_file);
 
                 // Print to stdout as well
                 printf("%s 0x%04X (%d)\n",
-                       (ev.value == 1) ? "PRESSED" : ((ev.value == 0) ?  "RELEASED":"REPEATED"),
+                       (ev.value == 1) ? "PRESSED" : ((ev.value == 2) ? "REPEATED" : "RELEASED"),
                        ev.code, ev.code);
-                       
-            }
-           history.events[history.index] = ev;
+
+                // Update the input event history
+                history.events[history.index] = ev;
                 history.index = (history.index + 1) % HISTORY_SIZE;
 
                 // Check for termination condition ("E+X" sequence)
-                if (history.events[0].code == KEY_E && history.events[1].code == KEY_X) {
+                if (history.events[0].code == KEY_E && history.events[1].code == KEY_X &&
+                    history.events[0].value == 1 && history.events[1].value == 1) {
                     break; // Terminate the loop on "E+X"
                 }
-        
 
-        // Check for termination condition (E+X)
-        /*if (ev.type == EV_KEY && ev.value == 1 && ev.code == KEY_E) {
-            // Read the next event
-            bytesRead = read(fd, &ev, sizeof(struct input_event));
+                // Check for specific shortcuts
+                if (history.events[0].code == KEY_P && history.events[1].code == KEY_E &&
+                    history.events[0].value == 1 && history.events[1].value == 1 &&
+                    history.events[2].code == KEY_P && history.events[2].value == 0) {
+                    print_custom_message("I passed the Exam!");
+                    // Reset the history
+                    history.index = 0;
+                }
 
-            // Check if the next event is 'X'
-            if (bytesRead != -1 && ev.type == EV_KEY && ev.value == 1 && ev.code == KEY_X) {
-                break; // Terminate the loop on 'E+X'
+                if (history.events[0].code == KEY_C && history.events[1].code == KEY_A &&
+                    history.events[2].code == KEY_P && history.events[3].code == KEY_P &&
+                    history.events[0].value == 1 && history.events[1].value == 1 &&
+                    history.events[2].value == 1 && history.events[3].value == 0) {
+                    print_custom_message("Get some cappuccino!");
+                    // Reset the history
+                    history.index = 0;
+                }// Check for custom shortcut
+                if (history.events[0].code == KEY_LEFTCTRL &&
+                    history.events[1].code == KEY_LEFTALT &&
+                    history.events[2].code == KEY_LEFTSHIFT &&
+                    history.events[3].code == KEY_F12 &&
+                    history.events[0].value == 1 &&
+                    history.events[1].value == 1 &&
+                    history.events[2].value == 1 &&
+                    history.events[3].value == 0) {
+                    print_custom_message("Custom shortcut activated: Ctrl+Alt+Shift+F12");
+                    // Reset the history
+                    history.index = 0;
+                }
             }
-        }*/}
+        }
     }
 
     // Close the keyboard device and output file
